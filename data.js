a185513d2eaf5d9e370ab20ebb8a7795fd299cf4
J-ATJ <98430219+J-ATJ@users.noreply.github.com>
2024-04-01 23:57:15 -0600

    Se agregó ventana modal con info del candidato

--- data.js
+++ data.js
@@ -5,56 +5,64 @@ const data = [
         "elo": 2758,
         "img": "ian-nepomniachtchi",
         "country": "Russia",
-        "info": ""
+        "info": "GM Ian Nepomniachtchi (“Nepo” for short) is a Russian super grandmaster who has twice won the Candidates Tournament to play for the world championship. After winning the 2020-21 Candidates Tournament, he challenged GM Magnus Carlsen for the 2021 World Championship and lost in 11 games. Nepomniachtchi then won the 2022 Candidates Tournament and ultimately faced then fell in tiebreaks to GM Ding Liren in 2023 World Championship.",
+        "url": "https://www.chess.com/players/ian-nepomniachtchi"
     },
     {
         "name": "Fabiano Caruana",
         "elo": 2803,
         "img": "fabiano-caruana",
         "country": "USA",
-        "info": ""
+        "info": "The prodigy who broke the record held by GM Hikaru Nakamura for America's youngest chess grandmaster, Fabiano Caruana, has climbed the mountain of chess and reached the summit. This grandmaster has had a ranking as high as number-two in the world and has won numerous tournaments in his career. In 2018 the American faced GM Magnus Carlsen at the World Chess Championship in London in 2018, losing in the tiebreak playoffs. He is also a three-time U.S. chess champion (2016, 2022, 2023)",
+        "url": "https://www.chess.com/players/fabiano-caruana"
     },
     {
         "name": "Hikaru Nakamura",
         "elo": 2789,
         "img": "hikaru-nakamura",
         "country": "USA",
-        "info": ""
+        "info": "Hikaru Nakamura was born December 9, 1987 in Hirakata, Japan. His family moved to the United States when he was just two years old, and the Stars and Stripes are the only national banner he has known as a chess player. He is a five-time U.S. champion, claiming the title in 2005, 2009, 2012, 2015, and 2019.",
+        "url": "https://www.chess.com/players/hikaru-nakamura"
     },
     {
         "name": "Vidit Santosh Gujrathi",
         "elo": 2742,
         "img": "vidit-gujrathi",
         "country": "India",
-        "info": ""
+        "info": "Vidit Gujrathi is an Indian super GM who has won the under-14 world championship, 2018 Tata Steel Challengers, and the 2019 Biel tournament. In 2023 he became a candidate for the world championship for the first time.",
+        "url": "https://www.chess.com/players/vidit-santosh-gujrathi"
     },
     {
         "name": "Alireza Firouzja",
         "elo": 2727,
         "img": "alireza-firouzja",
         "country": "France",
-        "info": ""
+        "info": "Alireza Firouzja is an Iranian-born grandmaster who now plays for France. He is a two-time world championship candidate and two-time Iranian champion. In late 2019 and early 2020, Firouzja electrified the chess world with his second place finish in the World Rapid Championship (one point behind World Champion Magnus Carlsen) Firouzja also broke the record for youngest player to surpass a 2800 rating.",
+        "url": "https://www.chess.com/players/alireza-firouzja"
     },
     {
         "name": "Gukesh Dommaraju",
         "elo": 2743,
         "img": "gukesh-dommaraju",
         "country": "India",
-        "info": ""
+        "info": "Indian GM Gukesh Dommaraju is a young chess prodigy. He’s best known for becoming the second-youngest grandmaster in history. The accomplishment came in January 2019, when Gukesh earned the GM title at 12 years, 7 months and 17 days of age—narrowly missing GM Sergey Karjakin’s record by 17 days. Gukesh is one of India’s brightest chess talents. In July 2022, he achieved a live rating of 2700, the fourth-youngest player to ever do so.",
+        "url": "https://www.chess.com/players/gukesh-dommaraju"
     },
     {
         "name": "Rameshbabu Praggnanandhaa",
         "elo": 2747,
         "img": "rameshbabu-praggnanandhaa",
         "country": "India",
-        "info": ""
+        "info": "GM Praggnanandhaa Rameshbabu is an Indian grandmaster who had one of the greatest youth careers in chess history. Praggnanandhaa, who only turned 18 years old in 2023, is one of the youngest grandmasters and world championship candidates ever. He is the first of a historic quartet of contemporary young Indian players—that also includes GMs Gukesh D, Arjun Erigaisi, and Nihal Sarin—to qualify for the Candidates Tournament, doing so months before Gukesh qualified for the same 2024 Candidates.",
+        "url": "https://www.chess.com/players/praggnanandhaa-rameshbabu"
     },
     {
         "name": "Nijat Abasov",
         "elo": 2632,
         "img": "nijat-abasov",
         "country": "Azerbaijan",
-        "info": ""
+        "info": "Nijat Abasov is an Azerbaijani grandmaster and the 2017 Azerbaijani Chess Champion. That year he won the Baku Open tournament, and another notable tournament win came at the 2016 Zurich Christmas Open. Abasov became a GM at the age of 15 and entered the list of the top-100 players in the world in November 2019. He has ranked as high number three on the list of players in Azerbaijan.",
+        "url": "https://www.chess.com/players/nijat-abasov"
     }
 ]
 
